1ddec8fb6bddb4e97f7358b3f119f6f1fa84599e
auyen <auyen@ucsc.edu>
2015-02-02 22:17:06 -0800

    generates xml with concept, prefLabel, altLabel

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,90 +1,101 @@
 # This program retrieves data from a specific spreadsheet, places it in a CSV file, and prints out information on a specific platform
+import gdata.docs.service
 import gspread
+import codecs
+import sys
 from getpass import getpass
-from platform_classes import Platform
-import rdflib
+from platform_class import Platformclass
+from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
 import csv
 import os.path
-
+import logging
+logging.basicConfig()
 allPlatforms = []
 response = ''
 if os.path.isfile("platformlist.csv"):
     response = raw_input("A CSV file exists. Would you like to use it? (Y/N)")
-
 if response == 'Y' or response == 'y':
     f = open('platformlist.csv')
     readCSV = csv.reader(f)
-    for x in readCSV:
-        print x[0]
-        allPlatforms.append(Platform(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9]))
 
-elif not os.path.isfile('platformlist.csv') or response == 'n' or response == 'N':    
+    for x in readCSV:
+        try:
+            print x[0]
+            allPlatforms.append(Platformclass(x[0],x[1],x[2],x[3],x[4],x[5],x[6]))
+        except IndexError:
+            break
+elif not os.path.isfile('platformlist.csv') or response == 'n' or response == 'N':
     username = raw_input('Enter Google User Name: ')
     passwd = getpass('Enter password: ')
     print 'authenticating with google...'
-
-    # Authenticate using your Google Docs email address and password.
-
+# Authenticate using your Google Docs email address and password.
     client = gspread.login(username + '@gmail.com', passwd)
-
-    # Query the server for the spreadsheet and worksheet
-    worksheet = client.open("Platform and Media Format Spreadsheet").get_worksheet(4)
+# Query the server for the spreadsheet and worksheet
+    sh = client.open_by_url("https://docs.google.com/spreadsheets/d/1zTDX9LNb5yR88IdvMbauBvCZL_TQPkMzompQz5QrRec/edit#gid=0")
+    worksheet = sh.get_worksheet(0)
     print 'organizing worksheet...'
-
-    # For the worksheet
+# For the worksheet
     platformList = worksheet.get_all_records()
-    platform_sheet_headers = ['Platform Name',
-                              'Alternate Name',
-                              'Alternate Version',
-                              'Media Formats',
-                              'Operating System (if applicable)',
-                              'Peripherals',
-                              'Sources',
-                              'Exclusion Rational',
-                              'Problematic',
-                              'Notes']
-    
+    platform_sheet_headers = ['skos:Concept',
+                              'skos:prefLabel',
+                              'skos:altLabel',
+                              'skos:definition',
+                              'skos:related',
+                              'skos:broader',
+                              'skos:note']
     for x in platformList:
-        print x['Platform Name']
-        platformObj = Platform(*[x[header] for header in platform_sheet_headers])
+        print x['skos:Concept']
+        platformObj = Platformclass(*[x[header] for header in platform_sheet_headers])
         allPlatforms.append(platformObj)
-
-    # Print all info to csv file
+# Print all info to csv file
     file = open("platformlist.csv", "wb")
     wri = csv.writer(file, quoting=csv.QUOTE_ALL)
     wri.writerow(platform_sheet_headers)
     for x in allPlatforms:
         wri.writerow(x.toStringCSV())
-        
-# Create RDF
+
+#XML
+    g = Graph()
+    skos = Namespace('http://www.w3.org/2004/02/skos/core#')
+    g.bind('skos', skos)
+    skip = 0
+
     for x in allPlatforms:
-        g.add((URIRef(x.platform_name), RDF['type'], skos['Concept']))
-        g.add((URIRef('URI'), skos['prefLabel'], Literal(x.platform_name, lang='en')))
-        lower = 0
-        morethanone = false
-        for y in x.alternate_name:
-            if x.alternate_name[y] == ';':
-                morethanone = true
-                lower = y+1
-                g.add((URIRef('URI'), skos['altLable'], URIRef(x.alternate_name[lower:y-1])))
-        if morethanone == false:
-            g.add((URIRef('URI'), skos['altLable'], URIRef(x.alternate_name)))
-    g.serialize('text.xml',format="pretty-xml")
-    
-
-
+        y = 0
+        morethanone = False
+        skip+=1
+        if skip == 1:
+            continue
+        g.add((URIRef(x.concept), RDF.type, skos['Concept']))
+        g.add((URIRef(x.concept), skos['prefLabel'], Literal(x.pref_label)))
+        if x.alt_label == '':
+            continue
+        if ';' in x.alt_label:
+            morethanone = True
+            count = 0
+            lower = 0
+            while y < len(x.alt_label):
+                if x.alt_label[y] == ';':
+                    count +=1
+                    if count == 1:
+                        g.add((URIRef(x.concept), skos['altLabel'], Literal(x.alt_label[0:y])))
+                
+                    if count > 1:
+                        g.add((URIRef(x.concept), skos['altLabel'], Literal(x.alt_label[lower:y])))
+                    lower = y+2
+                y +=1
+            g.add((URIRef(x.concept), skos['altLabel'], Literal(x.alt_label[lower:len(x.alt_label)])))
+        if morethanone == False:
+            g.add((URIRef(x.concept), skos['altLabel'], Literal(x.alt_label)))
+    g.serialize(destination = 'text.xml',format="pretty-xml")
 # Enter the name of the platform
 platformName = raw_input('Enter the name of a platform: ')
-
-
 # Put the information into a text file named the same as the platform
 platformindex = 0
 for x in allPlatforms:
-    if x.platform_name == platformName:
+    if x.concept == platformName:
         break
     else:
         platformindex += 1
-
-
 allPlatforms[platformindex].toFile()
 print 'File created'
